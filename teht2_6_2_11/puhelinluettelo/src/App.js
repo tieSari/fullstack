1f174c60afb59f393ef777dcb088efe4641446bd
Sari R <sariraut@dhcp-asv-101.helsinki.fi>
2018-01-27 18:31:41 +0200

    teht2_11

--- teht2_6_2_11/puhelinluettelo/src/App.js
+++ teht2_6_2_11/puhelinluettelo/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Persons from './components/Persons'
+import axios from 'axios'
 
 
 class App extends React.Component {
 constructor(props) {
   super(props)
   this.state = {
-    persons: [
-      { name: 'Arto Hellas', number: '040-123456' },
-      { name: 'Martti Tienari', number: '040-123456' },
-      { name: 'Arto Järvinen', number: '040-123456' },
-      { name: 'Lea Kutvonen', number: '040-123456' }
-    ],
+//    persons: [
+ //     { name: 'Arto Hellas', number: '040-123456' },
+ //     { name: 'Martti Tienari', number: '040-123456' },
+ //     { name: 'Arto Järvinen', number: '040-123456' },
+ //     { name: 'Lea Kutvonen', number: '040-123456' }
+ //   ],
+    persons: [],
     newName: '',
     newNumber: '',
     filter: ''
@@ -50,6 +52,15 @@ addPerson = (event) => {
   })
   }
 
+componentWillMount() {
+    console.log('will mount')
+    axios
+      .get('http://localhost:3002/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        this.setState({ persons: response.data })
+      })
+  }
 
 
   render() {
