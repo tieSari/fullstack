1a1a239a0026d0dfb828962de8ec63615f04c712
Sari R <sariraut@dhcp-asv-101.helsinki.fi>
2018-02-16 19:28:07 +0200

    teht4_4

--- teht4/utils/list_helper.js
+++ teht4/utils/list_helper.js
@@ -1,7 +1,12 @@
 const dummy = (blogs) => {
-    return 1;
-  }
-  
-  module.exports = {
-    dummy
-  }
+  return 1
+}
+
+
+const totalLikes = (blogs) => {
+  return blogs.length === 0 ? 0 : blogs.map(blog => blog.likes).reduce((a,b) => {return a + b})
+}
+
+module.exports = {
+  dummy, totalLikes
+}
