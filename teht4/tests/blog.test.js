c3158d6bcbc241f45568d046e196dd380bc01097
Sari R <sariraut@dhcp-asv-101.helsinki.fi>
2018-02-16 20:01:05 +0200

    teht4_5

--- teht4/tests/blog.test.js
+++ teht4/tests/blog.test.js
@@ -1,5 +1,32 @@
 const listHelper = require('../utils/list_helper')
 
+const listWithManyBlogs = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f9',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 1,
+    __v: 1
+  },
+  {
+    _id: '5a422aa71b54a676234d17f7',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 90,
+    __v: 0
+  }
+]
+
 test('dummy is called', () => {
   const blogs = []
 
@@ -25,32 +52,6 @@ describe('total_likes', () => {
     expect(result).toBe(5)
   })
 
-  const listWithManyBlogs = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    },
-    {
-      _id: '5a422aa71b54a676234d17f9',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 1,
-      __v: 1
-    },
-    {
-      _id: '5a422aa71b54a676234d17f7',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 90,
-      __v: 0
-    }
-  ]
 
   test('when list has many blogs equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithManyBlogs)
@@ -63,5 +64,22 @@ describe('total_likes', () => {
     const result = listHelper.totalLikes(listWithNoBlogs)
     expect(result).toBe(0)
   })
+
+})
+
+describe('favoriteBlog', () => {
+
+  test('return blog with most likes', () => {
+    const result = listHelper.favoriteBlog(listWithManyBlogs)
+    expect(result).toEqual({
+      _id: '5a422aa71b54a676234d17f7',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 90,
+      __v: 0
+    })
+  })
+
 
 })
--- teht4/utils/list_helper.js
+++ teht4/utils/list_helper.js
@@ -7,6 +7,12 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.map(blog => blog.likes).reduce((a,b) => {return a + b})
 }
 
+const favoriteBlog = (blogs) => {
+  const maxi =  Math.max(...blogs.map(blog => blog.likes))
+  console.log('suurin',maxi)
+  return blogs.length === 0 ? {} : blogs.filter(blog => blog.likes === maxi)[0]
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
