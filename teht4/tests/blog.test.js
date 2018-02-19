fab33fad19db17818939f41e74c8441c1cfac2ea
Sari R <sariraut@dhcp-asv-101.helsinki.fi>
2018-02-19 20:52:53 +0200

    teht4_6

--- teht4/tests/blog.test.js
+++ teht4/tests/blog.test.js
@@ -1,10 +1,23 @@
 const listHelper = require('../utils/list_helper')
 
-const listWithManyBlogs = [
+const listWithNoBlogs = []
+
+const listWithOneBlog = [
   {
     _id: '5a422aa71b54a676234d17f8',
     title: 'Go To Statement Considered Harmful',
     author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
+
+const listWithManyBlogs = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dejkstra',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5,
     __v: 0
@@ -36,16 +49,7 @@ test('dummy is called', () => {
 
 
 describe('total_likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
+
 
   test('when list has only one blog equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
@@ -58,7 +62,6 @@ describe('total_likes', () => {
     expect(result).toBe(96)
   })
 
-  const listWithNoBlogs = []
 
   test('when list has no blogs equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithNoBlogs)
@@ -79,6 +82,30 @@ describe('favoriteBlog', () => {
       likes: 90,
       __v: 0
     })
+  })
+})
+
+describe('whoIsWrittenTheMostBlogs', () => {
+
+  test('return most blogs written writer', () => {
+    const result = listHelper.mostBlogs(listWithManyBlogs)
+    expect(result).toEqual({
+      author: 'Edsger W. Dijkstra',
+      blogs: 2
+    })
+  })
+
+  test('return most blogs written writer one blog', () => {
+    const result = listHelper.mostBlogs(listWithOneBlog)
+    expect(result).toEqual({
+      author: 'Edsger W. Dijkstra',
+      blogs: 1
+    })
+  })
+
+  test('return most blogs written writer no blog', () => {
+    const result = listHelper.mostBlogs(listWithNoBlogs)
+    expect(result).toEqual({})
   })
 
 
--- teht4/utils/list_helper.js
+++ teht4/utils/list_helper.js
@@ -13,6 +13,31 @@ const favoriteBlog = (blogs) => {
   return blogs.length === 0 ? {} : blogs.filter(blog => blog.likes === maxi)[0]
 }
 
+//https://www.consolelog.io/group-by-in-javascript/ mukaeltuna
+Array.prototype.groupBy = function(prop) {
+  return this.reduce(function(groups, item) {
+    const val = item[prop]
+    var found = groups.find(function(item) {
+      return item.author === val
+    })
+    if(found===undefined){
+      groups.push({ 'author':item.author, 'blogs':1 })
+    }
+    else{
+      found.blogs++
+    }
+    return groups
+  }, [])
+}
+
+const mostBlogs = (blogs) => {
+  const groupedBlogs = blogs.groupBy('author')
+  console.log(groupedBlogs)
+  const maxi =  Math.max(...groupedBlogs.map(blog => blog.blogs))
+  console.log('suurin',maxi)
+  return blogs.length === 0 ? {} : groupedBlogs.filter(blog => blog.blogs === maxi)[0]
+}
+
 module.exports = {
-  dummy, totalLikes, favoriteBlog
+  dummy, totalLikes, favoriteBlog, mostBlogs
 }
